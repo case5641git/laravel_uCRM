b8dba640cd4d3b53830dfe70cc6cb3043fd50eed
case5641git <t.hiroto.job@mail.com>
2022-11-20 22:49:42 +0900

    inertia-vue.js

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,22 @@
+<template>
+    <GuestLayout>
+        <Label value="件名">タイトル</Label>
+        <Input
+            modelValue="初期値が入ります"
+            @update:modelValue="emitTest"
+        ></Input>
+    </GuestLayout>
+
+</template>
+
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue'
+import Label from '@/Components/InputLabel.vue'
+import Input from '@/Components/TextInput.vue'
+
+const emitTest = e => console.log(e)
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/Inertia/Show.vue
@@ -0,0 +1,26 @@
+<template>
+    {{ id }}<br>
+    {{blog.title}}
+    <br>
+    <button @click="deleteConfirm(blog.id)">削除</button>
+</template>
+
+<script setup>
+import {Inertia} from "@inertiajs/inertia";
+
+defineProps({
+    id : String,
+    blog: Object
+})
+
+const deleteConfirm = id => {
+    //console.log(id)
+    Inertia.delete(`/inertia/${id}`, {
+        onBefore: () => confirm('本当に削除しますか')
+    })
+}
+</script>
+
+<style scoped>
+
+</style>
